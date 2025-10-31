a5495c8426a62bb5766bcc151e941683966008c3
tenzou92 <tenzoucomp@gmail.com>
2025-10-31 14:44:25 +0800

    fix sidebar

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -1,45 +1,3 @@
-<template>
-  <div
-    class="sidebar"
-    :data-color="sidebarItemColor"
-    :data-image="sidebarBackgroundImage"
-    :style="sidebarStyle"
-  >
-    <div class="logo">
-      <a href="#" class="simple-text logo-mini">
-        <div class="logo-img" style="width: 100; height: 100">
-          <img
-            :src="imgLogo"
-            alt="App Logo"
-            style="width: 100px; height: 45px; object-fit: cover"
-          />
-        </div>
-      </a>
-
-      <a
-        href="http://localhost:8080/#/dashboard"
-        class="simple-text logo-normal"
-      >
-        {{ title }}
-      </a>
-    </div>
-    <div class="sidebar-wrapper">
-      <slot name="content"></slot>
-      <md-list class="nav">
-        <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
-        <slot>
-          <sidebar-link
-            v-for="(link, index) in sidebarLinks"
-            :key="link.name + index"
-            :to="link.path"
-            :link="link"
-          >
-          </sidebar-link>
-        </slot>
-      </md-list>
-    </div>
-  </div>
-</template>
 <script>
 import SidebarLink from "./SidebarLink.vue";
 
@@ -91,6 +49,32 @@ export default {
   },
 };
 </script>
+<template>
+  <div class="sidebar" :data-color="sidebarItemColor" :data-image="sidebarBackgroundImage" :style="sidebarStyle">
+    <div class="logo">
+      <a href="#" class="simple-text logo-mini">
+        <div class="logo-img" style="width: 100; height: 100">
+          <img :src="imgLogo" alt="App Logo" style="width: 100px; height: 45px; object-fit: cover" />
+        </div>
+      </a>
+
+      <a href="http://localhost:8080/#/dashboard" class="simple-text logo-normal">
+        {{ title }}
+      </a>
+    </div>
+    <div class="sidebar-wrapper">
+      <slot name="content"></slot>
+      <md-list class="nav">
+        <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
+        <slot>
+          <sidebar-link v-for="(link, index) in sidebarLinks" :key="link.name + index" :to="link.path" :link="link">
+          </sidebar-link>
+        </slot>
+      </md-list>
+    </div>
+  </div>
+</template>
+
 <style>
 @media screen and (min-width: 991px) {
   .nav-mobile-menu {
